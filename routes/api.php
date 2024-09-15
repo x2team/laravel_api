233d6c4ac3c3c55801cb0d662f7fa9afbe3b402a
BeeSupper <beesupper@BeeSuppers-MacBook-Pro.local>
2024-09-15 20:58:17 +0700

    24. Protecting routes

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,15 @@ use App\Http\Controllers\Api\V1\TaskController;
 
 
 
-Route::prefix('v1')->group(function(){
+Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
+
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+    
 });
 
 
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::prefix('auth')->group(function(){
-    Route::post('/login', LoginController::class);
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
